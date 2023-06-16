46b2ca10d0b08d15c27a42f26eb104940381dba6
Kajiru <sdkfz181tiger@gmail.com>
2023-06-16 19:34:53 +0900

    Update

--- template_014/libs/utility.js
+++ template_014/libs/utility.js
@@ -1,34 +1,61 @@
 console.log("Hello Utility!!");
 
 //==========
-// プレイヤークラス
+// 画面外判定処理
 
-class Player extends enchant.Sprite{
+// 画面外判定
+function isOutside(scene, spr){
+    if(spr.centerX < 0) return true;
+    if(spr.centerY < 0) return true;
+    if(scene.width < spr.centerX) return true;
+    if(scene.height < spr.centerY) return true;
+    return false;
+}
 
-    constructor(w, h, scene){
-        super(w, h);
-        this.scene = scene;// シーン
-        this.backgroundColor = "white";
+//==========
+// マガジンの処理
+
+var bullets = [];// 未使用の弾を格納するマガジン
+
+// マガジンに弾を用意する
+function readyBullet(total){
+    for(let i=0; i<total; i++){
+        var bullet = new Sprite(2, 8);// 弾スプライト
+        bullet.backgroundColor = "silver";
+        bullet.moveX = 0;// 速度も0に!! <- 重要!!
+        bullet.moveY = 0;
+        bullets.push(bullet);// マガジンに追加
     }
+}
 
-    setPosX(x){
-        this.centerX = x;
-    }
+// マガジンから取り出す
+function getBullet(){
+    if(bullets.length <= 0) return null;// マガジンに無い場合はnull
+    return bullets.pop();// マガジンから1つ取り出す
+}
 
-    setPosY(y){
-        this.centerY = y;
-    }
+// マガジンに戻す
+function returnBullet(bullet){
+    bullet.centerX = Math.random() * 20;// とりま画面の左上
+    bullet.centerY = Math.random() * 20;
+    bullet.moveX   = 0;// 速度も0に!!
+    bullet.moveY   = 0;
+    bullet.clearEventListener(Event.ENTER_FRAME);
+    bullets.push(bullet);// マガジンに戻す
+}
 
-    startDance(){
-        // 定期的にどこかへ移動する
-        this.tl.clear();
-        this.tl.delay(32);
-        this.tl.then(function(){
-            let x = Math.random() * this.scene.width;
-            let y = Math.random() * this.scene.height;
-            this.setPosX(x);
-            this.setPosY(y);
-        });
-        this.tl.loop();
-    }
+//==========
+// 敵を作る処理
+
+// 敵を作る関数
+function createEnemy(w, h, color, x, y, dX, dY, f){
+    // 敵
+    var enemy = new Sprite(w, h);
+    enemy.backgroundColor = color;
+    enemy.x = x;
+    enemy.y = y;
+    enemy.tl.moveBy(-dX, -dY, f);
+    enemy.tl.moveBy(dX, dY, f);
+    enemy.tl.loop();
+    return enemy;
 }
--- template_014/main.js
+++ template_014/main.js
@@ -1,6 +1,7 @@
 
 var assets = [
     "images/title.png",// タイトル
+    "images/bkg_320x480.png"
 ];
 
 function gameStart(){
@@ -13,18 +14,104 @@ function gameStart(){
     
     scene.backgroundColor = "gray";
 
+    readyBullet(100);// マガジンの準備
+
+    // 背景スクロール
+    var bkg = new Sprite(320, 480);
+    bkg.image = core.assets["images/bkg_320x480.png"];
+    bkg.x = 0;
+    bkg.y = 0;
+    scene.addChild(bkg);
+
+    var counter = 0;// カウンター
+    bkg.tl.moveTo(0, 480, 128);
+    bkg.tl.then(function(){
+        counter++;// カウントアップ
+        bkg.y = -480;// 背景を上に戻す
+
+         // 新敵登場(カウンターの数だけ増える)
+        for(let i=0; i<counter; i++){
+            var x = Math.random() * scene.width;
+            var y = Math.random() * 200;
+            enemyGroup.addChild(
+                createEnemy(32, 32, "orange",
+                    x, y, 40, 40, 32));
+        }
+    });
+    bkg.tl.loop();
+
     // プレイヤー
-    var player = new Player(16, 16, scene);
-    player.setPosX(scene.width / 2);
-    player.setPosY(scene.height / 2);
+    var player = new Sprite(16, 16);
+    player.backgroundColor = "white";
+    player.centerX = scene.width / 2 - 100;
+    player.centerY = scene.height - 100;
     scene.addChild(player);
 
-    scene.on(Event.TOUCH_START, function(e){
-        console.log(e.x, e.y);
-        player.setPosX(e.x);
-        player.setPosY(e.y);
-        //player.startDance();
+    player.tl.moveBy(200, 0, 64);
+    player.tl.moveBy(-200, 0, 64);
+    player.tl.loop();
+
+    var enemyGroup = new Group();// 敵グループ
+    scene.addChild(enemyGroup);
+
+    var bltGrpPlayer = new Group();// 弾グループ(プレイヤー)
+    scene.addChild(bltGrpPlayer);
+
+    var bltGrpEnemy  = new Group();// 弾グループ(敵)
+    scene.addChild(bltGrpEnemy);
+
+    // 射撃開始(プレイヤー)
+    bltGrpPlayer.tl.delay(4);
+    bltGrpPlayer.tl.then(function(){
+        var bullet = getBullet();
+        if(!bullet) return;
+        bullet.backgroundColor = "white";
+        bullet.centerX = player.centerX;
+        bullet.centerY = player.centerY;
+        bullet.moveX   = 0;
+        bullet.moveY   = -8;
+
+        bullet.on(Event.ENTER_FRAME, function(){
+            this.centerX += this.moveX;// 移動
+            this.centerY += this.moveY;
+
+            // 画面外
+            if(isOutside(scene, this)){
+                returnBullet(this);// マガジンに戻す
+                return;
+            }
+
+            // 敵グループと衝突
+            for(var enemy of enemyGroup.childNodes){
+                if(bullet.intersect(enemy)){
+                    returnBullet(this);// マガジンに戻す
+                    return;
+                }
+            }
+        });
+        bltGrpPlayer.addChild(bullet);// グループに追加
     });
+    bltGrpPlayer.tl.loop();
+
+    /*
+    // 敵1
+    enemyGroup.addChild(
+        createEnemy(32, 32, "red",
+            scene.width / 2 - 40, scene.height * 0.2,
+            40, 40, 32));
+
+    // 敵2
+    enemyGroup.addChild(
+        createEnemy(32, 32, "green",
+            scene.width / 2 + 0, scene.height * 0.2 + 40,
+            30, 20, 24));
+
+    // 敵3
+    enemyGroup.addChild(
+        createEnemy(32, 32, "blue",
+            scene.width / 2 + 40, scene.height * 0.2,
+            20, 30, 16));
+    */
 
     //==========
     // ここまで
