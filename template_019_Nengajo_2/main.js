81a3a52dcc0803b02a03379a96addfb4208e67b1
Kajiru <sdkfz181tiger@gmail.com>
2023-12-09 08:30:51 +0900

    Update

---
+++ template_019_Nengajo_2/main.js
@@ -0,0 +1,103 @@
+
+var assets = [
+    "images/title.png",// タイトル
+    "images/bkg_320x480.png"
+];
+
+function gameStart(){
+    var scene = new Scene();
+    core.replaceScene(scene); core.resume();
+
+    //==========
+    // ここから
+    //==========
+    
+    scene.backgroundColor = "whitesmoke";
+
+    // 中心座標
+    var cX = scene.width/2;
+    var cY = scene.height/2;
+    var pX = 50;
+    var color = "firebrick";
+
+    // 4つのラベル
+    var lbl1 = createLabel(scene, "2", cX-pX*1.7, cY, 64, color);
+    scene.addChild(lbl1);
+    var lbl2 = createLabel(scene, "0", cX-pX*0.7, cY, 64, color);
+    scene.addChild(lbl2);
+    var lbl3 = createLabel(scene, "2", cX+pX*0.3, cY, 64, color);
+    scene.addChild(lbl3);
+    var lbl4 = createLabel(scene, "4", cX+pX*1.3, cY, 64, color);
+    scene.addChild(lbl4);
+
+    var msg = createLabel(scene, " HAPPY NEW YEAR!!", cX, cY+80, 16, "black");
+    scene.addChild(msg);
+
+    // スプライト
+    var nasu = new Sprite(32, 32);
+    nasu.backgroundColor = "purple";
+    nasu.x = cX - nasu.width/2;
+    nasu.y = cY - 80;
+    scene.addChild(nasu);
+
+    nasu.tl.rotateBy(360, 64);
+    nasu.tl.then(function(){
+        console.log("end");
+    });
+    nasu.tl.loop();
+
+    jumpLabel(lbl1);
+    // jumpLabel(lbl2);
+    // jumpLabel(lbl3);
+    // jumpLabel(lbl4);
+    
+    //==========
+    // ここまで
+    //==========
+};
+
+function createLabel(scene, text, x, y, size, color){
+    var lbl = new Label(text);
+    lbl.x = x - lbl._boundWidth / 2;
+    lbl.y = y - lbl._boundHeight / 2;
+    lbl.color = color;
+    lbl.font = size + "px 'PixelMplus10'";
+    return lbl;
+}
+
+function jumpLabel(lbl, height=50, frame=4, cnt=3){
+    lbl.tl.moveBy(0, -height, frame, enchant.Easing.QUAD_EASEOUT);
+    lbl.tl.moveBy(0, height, frame, enchant.Easing.QUAD_EASEIN);
+    lbl.tl.delay(8);
+    lbl.tl.then(function(){
+        console.log("end");
+        cnt--;
+        if(cnt <= 0){
+            lbl.tl.unloop();
+            lbl.tl.clear();
+            console.log(lbl.tl);
+            //jumpLabel(lbl);
+        }
+    });
+    lbl.tl.loop();
+}
+
+function titleStart(){// タイトル画面
+    var scene = gameManager.createTitleScene();
+    core.replaceScene(scene); core.pause();
+    scene.on(enchant.Event.TOUCH_START, function(){gameStart();});
+}
+
+//==========
+//EnchantJS
+enchant();
+var gameManager;
+var core;
+var scene;
+window.onload = function(){
+    gameManager = new common.GameManager();
+    core = gameManager.createCore(320, 480);
+    core.preload(assets);
+    core.onload = function(){titleStart();};
+    core.start();
+};
